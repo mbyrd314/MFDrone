8a2e3e1f15c0b8086df05e288a6d12ba7bd9578b
Samvruta Tumuluru <tsamvrutha@gmail.com>
2018-01-21 17:56:09 -0600

    HSV color detection and Circle Detection

--- DroneDetect.py
+++ DroneDetect.py
@@ -1,26 +1,46 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Begin video feed
 cap = cv2.VideoCapture(0)
 
-#Resize template
-template = cv2.imread('DroneDesign.jpeg', 0)
-template= cv2.resize(template, (0,0), fx =.05, fy =.05)
 
+ret, frame = cap.read()
+hsv_template = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+plt.imshow(hsv_template)
+plt.show()
 
-#creating feature detection object
-orb = cv2.ORB_create()
-kp1, des1 = orb.detectAndCompute(template,None)
+#if cv2.waitKey(0) & 0xFF == ord('c'):
+#    x = raw_input()
+#    y = raw_input()
+
+#hue, saturation, value = hsv_template[y,x,:]
+#print("HUE: %d /t Saturation: %d /t Value: %d" %(hue, saturation,value))
 
 while True:
-    #Filter frame and change to grayscale
+    #Filter frame
     ret, frame = cap.read()
     frame = cv2.medianBlur(frame,5)
     gframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    #Convert BGR to HSV
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    #define color range to be detectted
+    lower_val = np.array([100,100,50])
+    upper_val= np.array([120,170,255])
+
+    #Threshold HSV to get color of interest
+    mask = cv2.inRange(hsv, lower_val, upper_val)
+
+    #Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    gres = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+
     #Identify circles
-    circles = cv2.HoughCircles(gframe,cv2.HOUGH_GRADIENT,1,minDist=2000,param1=60,param2=35,minRadius=0,maxRadius=0)
+    circles = cv2.HoughCircles(gres,cv2.HOUGH_GRADIENT,1,minDist=20,param1=50,param2=30,minRadius=20,maxRadius=0)
     if circles is not None:
         circles = np.uint16(np.around(circles))
 
@@ -30,25 +50,11 @@ while True:
             # draw the center of the circle
             cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
 
-            #cv2.imshow('detected circles',frame)
+        #cv2.imshow('image 3', frame)
 
-            #define region of interest as square of side length 2r
-            x,y,radius = i[0],i[1],i[2]
-            roi_frame = frame[y-radius: y+radius, x-radius:x+radius]
-            roi_gframe = gframe[y-radius: y+radius, x-radius:x+radius]
-
-            #Create bf matcher
-            kp2, des2 = orb.detectAndCompute(gframe,None)
-            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = False)
-
-            #determine matches and sort
-            matches = bf.match(des1,des2)
-            matched = sorted(matches, key = lambda x:x.distance)
-            frame = cv2.drawMatches(template, kp1, frame, kp2, matches[:2], None, flags=2)
-        #display image with circle detection and feature match
-        cv2.imshow('image 3', frame)
-
-    #quit using q    
+    cv2.imshow('mask', mask)
+    cv2.imshow('video feed', frame)
+    #quit using q
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
